ff54da7e8ba673384a1d9952511491da831769e4
Camille Demasson <camille.demasson@gmail.com>
2018-03-27 12:27:55 +0200

    Beginning of CW2 - question 1-1a) is done. tsubuka10points.mat contains 10 interests points for the 5 pictures of the tsubuka sequence, manually selected.

---
+++ main.m
@@ -0,0 +1,24 @@
+%% creation of the boat and tsukuba dataset
+clear all;
+
+for FIG = 1:6
+    boat(FIG).fig = imread(['img' num2str(FIG) '.pgm']);
+end
+
+for FIG = 1:5
+    tsukuba(FIG).fig = imread(['scene1.row3.col' num2str(FIG) '.ppm']);
+end
+
+%% let's register some coordinates
+% in the tsubuka sequence, let's click on 10 identical points on the 5
+% pictures
+tsubuka_coor = zeros(2,5,10);
+for PT=1:10
+    for FIG=1:5
+        figure(FIG);
+        image(tsukuba(FIG).fig);
+        [tsubuka_coor(1, FIG, PT), tsubuka_coor(2, FIG, PT)] = ginput(1);
+    end
+end
+
+%% 
