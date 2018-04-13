d9114d1c4a21f58f782ec83b4e2f5a9d673259a0
David Rock Jedeikin <davidrockjedeikin@davids-mbp.home>
2018-04-13 14:15:36 +0100

    Stereo Complete

--- main.m
+++ main.m
@@ -15,15 +15,14 @@ for FIG = 1:5
     tsukuba(FIG).fig = imread(['scene1.row3.col' num2str(FIG) '.ppm']);
 end
 
-for FIG = 1:4
+for FIG = 1:2
     % READING FD IMAGES
     % SELECT WHICH FD IMAGES TO USE
-    FDimages(FIG).fig = imread(['RR' num2str(FIG) '.JPG']);  
+    FDimages(FIG).fig = imread(['scene1.row3.col' num2str(FIG) '.ppm']);  
     % Uncomment if you need to rotate the images
-    FDimages(FIG).fig = imrotate(FDimages(FIG).fig,-90);           
+    %FDimages(FIG).fig = imrotate(FDimages(FIG).fig,-90);           
 end
 
-clear vars 
 
 % *************************************************************************
 %%                MANUALLY MATCHING INTEREST POINTS
@@ -54,7 +53,7 @@ end
 
 %% FD SEQUENCE 
 FDcc = zeros(2,2,4);      % FD Corrosponding Coordinates
-Picture = [3,4];       % Select the pictures to compare
+Picture = [1,2];       % Select the pictures to compare
 Pic1 = Picture(1,1); 
 Pic2 = Picture(1,2); 
 for PT= 1:10
@@ -136,7 +135,7 @@ FA = Fund_Matrix_Accuracy(FM,FDcc);   % Estimating the accuracy
 % Calculate the maximum range in the X coordinate
 range = size(FDimages(1).fig);  
 range = range(1,2); 
-IPs = 1;                    % SELECT THE INTEREST POINTS TO COMPARE
+IPs = 5;                    % SELECT THE INTEREST POINTS TO COMPARE
 PointA = FDcc(:,1,IPs);
 PointB = FDcc(:,2,IPs); 
 EpipolarA = EpiLine(FM,PointA,true,range);
@@ -183,7 +182,7 @@ colormap(gca,jet)
 colorbar
 
 
-%% Rectify Images 
+%% Rectify Images Manual  
 CPointA = [];
 CPointB = [];
 for i = 1: length(FDcc) 
@@ -204,5 +203,101 @@ imshow(I2Rect)
 % imshow(stereoAnaglyph(I1Rect, I2Rect));
 % title('Rectified Stereo Images (Red - Left Image, Cyan - Right Image)');
 
+%% Rectify Images Matlab   
+
+% Set 1 to visualize and 0 else 
+visualize = 1; 
+
+%                   STEP 1:   Read stereo image pair  
+I1 = FDimages(1).fig;
+I2 = FDimages(2).fig; 
+% Convert to grayscale.
+I1gray = rgb2gray(I1);
+I2gray = rgb2gray(I2);
+
+if (visualize == 1)
+figure;
+imshowpair(I1, I2,'montage');
+title('I1 (left); I2 (right)');
+figure;
+imshow(stereoAnaglyph(I1,I2));
+title('Composite Image (Red - Left Image, Cyan - Right Image)');
+end 
+
+%                 STEP 2:   Collect interest points 
+
+blobs1 = detectSURFFeatures(I1gray, 'MetricThreshold', 2000);
+blobs2 = detectSURFFeatures(I2gray, 'MetricThreshold', 2000);
+
+if (visualize == 1)
+figure;
+imshow(I1);
+hold on;
+plot(selectStrongest(blobs1, 30));
+title('Thirty strongest SURF features in I1');
+
+figure;
+imshow(I2);
+hold on;
+plot(selectStrongest(blobs2, 30));
+title('Thirty strongest SURF features in I2');
+end 
+
+%               STEP 3:   Find point correspondences
+
+[features1, validBlobs1] = extractFeatures(I1gray, blobs1);
+[features2, validBlobs2] = extractFeatures(I2gray, blobs2);
+
+% Match featues using SAD
+indexPairs = matchFeatures(features1, features2, 'Metric', 'SAD','MatchThreshold', 5);
+
+matchedPoints1 = validBlobs1(indexPairs(:,1),:);
+matchedPoints2 = validBlobs2(indexPairs(:,2),:);
+
+if (visualize == 1)
+figure;
+showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
+legend('Putatively matched points in I1', 'Putatively matched points in I2');
+end 
+
+%            STEP 4:   Remove outliers using Epopolar Constraints
+
+[fMatrix, epipolarInliers, status] = estimateFundamentalMatrix(...
+  matchedPoints1, matchedPoints2, 'Method', 'RANSAC', ...
+  'NumTrials', 10000, 'DistanceThreshold', 0.1, 'Confidence', 99.99);
+
+if status ~= 0 || isEpipoleInImage(fMatrix, size(I1)) ...
+  || isEpipoleInImage(fMatrix', size(I2))
+  error(['Either not enough matching points were found or '...
+         'the epipoles are inside the images. You may need to '...
+         'inspect and improve the quality of detected features ',...
+         'and/or improve the quality of your images.']);
+end
+
+inlierPoints1 = matchedPoints1(epipolarInliers, :);
+inlierPoints2 = matchedPoints2(epipolarInliers, :);
+
+if (visualize == 1)
+figure;
+showMatchedFeatures(I1, I2, inlierPoints1, inlierPoints2);
+legend('Inlier points in I1', 'Inlier points in I2');
+end 
+
+%                   STEP 5:   Rectify Images
+
+[t1, t2] = estimateUncalibratedRectification(fMatrix, ...
+  inlierPoints1.Location, inlierPoints2.Location, size(I2));
+tform1 = projective2d(t1);
+tform2 = projective2d(t2);
+
+
+[I1Rect, I2Rect] = rectifyStereoImages(I1, I2, tform1, tform2);
+if (visualize == 1)
+figure;
+imshowpair(I1Rect, I2Rect,'montage');
+figure;
+imshow(stereoAnaglyph(I1Rect, I2Rect));
+title('Rectified Stereo Images (Red - Left Image, Cyan - Right Image)');
+end 
 
 
